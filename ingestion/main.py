eb2da2d99759a59079bd764a006b973af7a6d374
issa736 <issa24257328@gmail.com>
2026-04-29 12:29:13 -0700

    Update main.py

--- ingestion/main.py
+++ ingestion/main.py
@@ -1,3 +1,5 @@
+import requests # lets us talk to APIs
+import time # allows a pause
 import os
 from confluent_kafka import Producer
 from dotenv import load_dotenv
@@ -8,10 +10,24 @@ def main():
     print("Ingestion service started...")
     
     # TO DO: Add data source API connection
-
-    # TO DO: Add redpanda connection
+    url = "https://api.open-meteo.com/v1/forecast"     # Weather API 
     
-    pass
+    params = {
+        "latitude": 33.9533,
+        "longitude": -117.3962,
+        "current_weather": True
+    }
+
+    while True:
+        r = requests.get(url, params=params)
+
+        if r.status_code == 200:
+            print(r.json())
+        else:
+            print("Error:", r.status_code)
+
+        time.sleep(10)
+    # TO DO: Add redpanda connection
 
 if __name__ == "__main__":
     main()
