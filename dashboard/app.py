08b1a54163ff6ae1ab695ec54d6f9ce0dece3c1b
dennisWu67 <dwu0561@gmail.com>
2026-05-07 13:47:03 -0700

    visualization of predicted weather

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,24 @@
 import streamlit as st
 import psycopg2
+import pandas as pd
 import os
 
 st.title("Project Dashboard")
 st.write("Visualize data!")
 
 # TO DO: Add connection to postgres and visualizations
+
+conn = psycopg2.connect(
+    host="postgres",
+    database="project_db",
+    user="username",
+    password="password"
+)
+
+# Query data
+query = "SELECT * FROM predictions ORDER BY created_at DESC LIMIT 20;"
+df = pd.read_sql(query, conn)
+
+# Show data
+st.subheader("Latest Data")
+st.dataframe(df)
